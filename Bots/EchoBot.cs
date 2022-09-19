910c2b8613193196938f77b02960175e6e4d0709
Cinzia Cernetti <cinzia.cernetti@avanade.com>
2022-09-19 09:15:25 +0200

    add files in folder

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with EchoBot .NET Template version v4.17.1
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using AdaptiveCards.Templating;
+using AdaptiveCards;
+using System;
+using NugetJObject;
+using System.IO;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace EchoBot.Bots
+{
+    
+      
+    public class EchoBot : ActivityHandler
+    {
+          private readonly string card1 =Path.Combine(".",  "SolitaireCard.json");
+        private static Attachment CreateAdaptiveCardAttachment(string filePath)
+        {
+            var adaptiveCardJson = File.ReadAllText(filePath);
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Text.ToUpper()=="CLICKED") {var replyText = $"Great! Let me know, I'd love to have your feedback";
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);}
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {   
+             string card2 =Path.Combine(".",  "chiara.png");
+            var welcomeText = "Hi, welcome to Chiara World!!\n Follow the link below to find out some news about my EP coming out on September 26th. \n ";
+            foreach (var member in membersAdded)
+            {
+                     
+                        var cardAttachment = CreateAdaptiveCardAttachment(card1);
+                         await turnContext.SendActivityAsync(MessageFactory.Attachment(cardAttachment), cancellationToken);
+
+            //turnContext.Activity.Attachments = new List<Attachment>() { cardAttachment };
+               
+            }
+        }
+        
+
+            
+    }
+    
+    
+        
+}
+
